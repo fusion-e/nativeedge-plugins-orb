06c6dec1b938090b9cd55b552920a67869e4c557
earthmant <ttearthman@gmail.com>
2024-11-07 16:53:33 -0500

    [semver:patch]

--- src/scripts/wagon.sh
+++ src/scripts/wagon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 make compile
 make wheels
-wagon create -r requirements.txt --build-tag "manylinux" -vvv -f -a "--find-links /workspace/build" --output-directory ~/project/ ~/project/
+wagon create --add-file plugin.yaml -r requirements.txt --build-tag "manylinux" -vvv -f -a "--find-links /workspace/build" --output-directory ~/project/ ~/project/
